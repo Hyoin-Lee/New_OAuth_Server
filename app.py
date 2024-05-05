087ac99864ce51f3cbf663c2745f2b6b19bb0d13
winroof1988@gmail.com <Qlalf2ek!@#$>
2024-05-05 22:18:59 +0800

    index page updated (Greeting message with username, log in button, log out button)

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, request, session, redirect, url_for
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+app.secret_key = 'your_secret_key_here'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return f'Hello, stranger'
+    if request.method == 'POST':
+        if 'logout' in request.form:
+            session.pop('username', None)
+            return redirect(url_for('hello_world'))
+        username = request.form.get('username')
+        if username:
+            session['username'] = username
+            return (f'Hello, {username}! '
+                    f'<form action="" method="post"><input type="submit" name="logout" value="Logout"></form>')
+        else:
+            return 'Please provide a username!'
+    if 'username' in session:
+        return (f'Hello, {session["username"]}! '
+                f'<form action="" method="post"><input type="submit" name="logout" value="Logout"></form>')
+    return '''
+    <form action="" method="post">
+      <input type="text" name="username" placeholder="username">
+      <button type="submit">Login</button>
+    </form>
+    '''
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
